3dc1acd135c5f6723c5f1b488341387af72cae5f
Satyam Sharma <satyamkailashsharma@gmail.com>
2025-08-19 16:25:48 +0530

    deployment_testing_8.1

--- backend/src/trpc/trpc.ts
+++ backend/src/trpc/trpc.ts
@@ -7,17 +7,14 @@ export const router: typeof t.router = t.router;
 export const publicProcedure: typeof t.procedure = t.procedure;
 
 // Protected procedure with JWT auth
-const isAuthed = t.middleware(({ ctx, next }) => {
+export const protectedProcedure: typeof t.procedure = t.procedure.use(async ({ ctx, next }) => {
   if (!ctx.userId) {
     throw new TRPCError({ code: 'UNAUTHORIZED' });
   }
   return next({
     ctx: {
       ...ctx,
-      // We're passing the `userId` in the context, so we can access it in the resolvers
       userId: ctx.userId as string,
     },
   });
 });
-
-export const protectedProcedure: typeof t.procedure = t.procedure.use(isAuthed);
